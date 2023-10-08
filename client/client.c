12269642ba63b2582baa03b83aed988eed46acde
TaeHyun Yun <yunnina01@naver.com>
2023-10-08 21:07:35 +0900

    클라이언트 코드 주석 추가 및 텍스트 파일 수정

--- client/client.c
+++ client/client.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[]) {
     int next;
     char sec[5];
 
+	// Socket Connection
 	if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
 		errorHandling("WSAStartup() error!");
 	
@@ -35,21 +36,27 @@ int main(int argc, char *argv[]) {
 	if(connect(clientSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
 		errorHandling("connect() error!");
 	
+	// File Naming
     LOG_FILE[0] = '\0';
 	strcat(LOG_FILE, fileNaming(argv[1]));
 	fp = fopen(LOG_FILE, "w");
 
+	// Connection Logging
 	informConnection(fp, argv[1]);
 	
+	// Code for Generating Random Numbers
 	srand(time(NULL));
 
+	// Socket Communication
 	while(1){
+		// Receive Message
 		strLen = recv(clientSock, message, sizeof(message) - 1, 0);
 		if(strLen <= 0)
 			errorHandling("read() error!");
 
         systemClock[0] = message[strLen - 1] = clock[0] = '\0';
         
+		// Timeout Checking
 		if(!strncmp(message, "TIMEOUT", 7)){
             strcat(systemClock, message + 8);
 			informTermination(fp, systemClock, argv[1]);
@@ -58,8 +65,10 @@ int main(int argc, char *argv[]) {
 
         strncat(systemClock, message, 7);
 
+		// Message Reception Logging
 		informReceiving(fp, systemClock, message + 8);
 
+		// Calculation
 		answer = computeAnswer(message);
         itoa(answer, res, 10);
         next = rand() % 5;
@@ -68,10 +77,11 @@ int main(int argc, char *argv[]) {
 		message[0] = '\0';
         strcat(message, makeMessage(sec, res));
 		
+		// Send Message
 		send(clientSock, message, strlen(message) + 1, 0);
 
+		// Message Transmission Logging
         strcat(clock, addGetSystemClock(systemClock, next));
-
 		informSending(fp, clock, res);
 	}
 	
