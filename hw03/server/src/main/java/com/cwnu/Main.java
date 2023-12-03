2383c3430ca785c7b2b9e10c3aceb5f1cadfbbb5
jujuwon <webmon@kakao.com>
2023-12-03 19:03:04 +0900

    서버 구동 추가

--- hw03/server/src/main/java/com/cwnu/Main.java
+++ hw03/server/src/main/java/com/cwnu/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		System.out.println("Hello world!");
 		Server server = new Server();
+		server.start();
 	}
 }
